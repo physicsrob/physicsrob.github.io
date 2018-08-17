ad74ee32e107842d1aa2d47501b7cb88f441429a
Robert Porter <rob@gradecam.com>
2018-08-17 16:31:08 -0700

    Nikola auto commit.
    Nikola version: 7.8.15

--- conf.py
+++ conf.py
@@ -1230,6 +1230,17 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # in the default template (base.tmpl).
 # (translatable)
 # BODY_END = ""
+BODY_END = """
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-124168012-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+        gtag('config', 'UA-124168012-1');
+        </script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
